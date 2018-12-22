9b6e7f9e285eb763d03652903390c68d60e522be
Chitoku <odango@chitoku.jp>
2018-12-23 01:45:43 +0900

    Update dependencies

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ require.config({
         // Packages
         'babel-standalone': 'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min',
         'babel-polyfill': 'https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min',
-        'vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.6/vue',
+        'vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.21/vue',
         'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/3.0.1/vuex.min',
-        'vue-router': 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.0.1/vue-router.min',
+        'vue-router': 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.0.2/vue-router.min',
         // 'http-vue-loader': 'https://unpkg.com/http-vue-loader@1.3.4/src/httpVueLoader',
         'http-vue-loader': 'https://cdn.rawgit.com/chitoku-k/http-vue-loader/bc8e142332b8f396715ec3fc61fbad03ae2a8040/src/httpVueLoader',
     },
