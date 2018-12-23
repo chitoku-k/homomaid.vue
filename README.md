a0fdbec85635fcb9aaefd32d0e85813e9f35dde9
Chitoku <odango@chitoku.jp>
2018-12-24 00:26:38 +0900

    Use http-vue-loader@1.4.0

--- README.md
+++ README.md
@@ -8,8 +8,3 @@ http-vue-loader + babel-standalone + RequireJS
 
 Open [here](https://chitoku-k.github.io/homomaid.vue/) or clone repo and serve
 `index.html` via a Web server.
-
-# Tip
-
-The latest http-vue-loader can't support.
-Look at this PR [FranckFreiburger/http-vue-loader#36](https://github.com/FranckFreiburger/http-vue-loader/pull/36).
--- js/main.js
+++ js/main.js
@@ -10,8 +10,7 @@ require.config({
         'vue': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.21/vue',
         'vuex': 'https://cdnjs.cloudflare.com/ajax/libs/vuex/3.0.1/vuex.min',
         'vue-router': 'https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.0.2/vue-router.min',
-        // 'http-vue-loader': 'https://unpkg.com/http-vue-loader@1.3.4/src/httpVueLoader',
-        'http-vue-loader': 'https://cdn.jsdelivr.net/gh/chitoku-k/http-vue-loader@493ebca15832161dccf55c8a7b654fd8c5cd2ea6/src/httpVueLoader',
+        'http-vue-loader': 'https://unpkg.com/http-vue-loader@1.4.0/src/httpVueLoader',
     },
 });
 require(['babel!app']);
